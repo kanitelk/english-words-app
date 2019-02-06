f725cd30d374c5eb3360646da8da2bfb82ff546b
Kanitelk <derodroll@gmail.com>
2019-02-06 17:54:54 +0300

    Add docs (GH Pages)

---
+++ docs/precache-manifest.f407ba003e8e7679f2eb83093cf9253d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "d00f990588a18bc05811",
+    "url": "./static/js/main.d00f9905.chunk.js"
+  },
+  {
+    "revision": "1871d0982b9a205157d1",
+    "url": "./static/js/1.1871d098.chunk.js"
+  },
+  {
+    "revision": "d00f990588a18bc05811",
+    "url": "./static/css/main.5efcc34a.chunk.css"
+  },
+  {
+    "revision": "5b3ba3341b54d07b227a67e56e34c2a8",
+    "url": "./index.html"
+  }
+];
